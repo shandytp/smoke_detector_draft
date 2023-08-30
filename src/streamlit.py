4abe26de0cf083ba0f1ccc531f7d56a80779cae6
shandytp <shandytsalasa@gmail.com>
2023-08-30 14:17:44 +0700

    update streamlit host

--- src/streamlit.py
+++ src/streamlit.py
@@ -90,7 +90,7 @@ with st.form(key = "air_data_form"):
         # Create loading animation while predicting
         with st.spinner("Sending data to prediction server..."):
             # TODO: ganti pake nama service/ip address public AWS
-            res = requests.post("http://api:8080/predict", json = raw_data).json()
+            res = requests.post("http://api_backend:8080/predict", json = raw_data).json()
             
         # Parse the prediction result
         if res["error_msg"] != "":
